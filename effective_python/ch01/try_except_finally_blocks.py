15d46185f6989ad536b00c937942e9482cf102ab
Joe Dougherty <joseph.dougherty@gmail.com>
2017-01-02 23:50:11 -0500

    Adds try/finally example and try/except/else example

--- effective_python/ch01/try_except_finally_blocks.py
+++ effective_python/ch01/try_except_finally_blocks.py
@@ -2,3 +2,24 @@
 If you're going to use try/except/finally blocks, 
 learn how to use all the sections properly!
 """
+
+# finally always runs
+f = open('/path/to/file.txt')
+
+try:
+    content = f.read()
+finally:
+    f.close()
+    
+# else blocks can be used to
+# run code if the try code 
+# has *not* raised an exception
+
+try:
+    assert None is None
+except ValueError as e:
+    raise
+else:
+    print('this will run as the try block here is tautological')
+
+
