d85f0a5d95e33d7e707ec78d6ef68cbf3adecfc1
Joe Dougherty <joseph.dougherty@gmail.com>
2017-01-08 14:38:08 -0500

    Begin chapter 3

---
+++ effective_python/ch03/helper_classes_v_dicts_and_tuples.py
@@ -0,0 +1,11 @@
+"""
+
+If you find yourself in a situation where you want to wrap
+some complex behavior around a dict (or worse, dict of dicts),
+see if you can instead refactor it into a collection of small,
+easily understood classes.
+
+"""
+
+# TODO: provide concrete example
+
