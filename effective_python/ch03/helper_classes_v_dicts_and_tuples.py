a333e9dedd33484ca8ab00d546fdb5a73b40e14c
Joe Dougherty <joseph.dougherty@gmail.com>
2017-01-10 21:53:21 -0500

    Adds basic example demonstrating use of helper classes v. nested dicts

--- effective_python/ch03/helper_classes_v_dicts_and_tuples.py
+++ effective_python/ch03/helper_classes_v_dicts_and_tuples.py
@@ -7,5 +7,52 @@ easily understood classes.
 
 """
 
-# TODO: provide concrete example
+# Not too bad so far, but what it we want to include
+# a player position preference?
+foos = {'joe':
+            {'wins': 5, 'losses': 5},
+        'eric':
+            {'wins': 7, 'losses': 5},
+        'ryan':
+            {'wins': 9, 'losses': 3},
+        }
+
+# Adding pos preference...
+foos = {'joe':
+        {'wins': 5, 'losses': 5, 'pos': 'goalie'},
+        'eric':
+        {'wins': 7, 'losses': 5, 'pos': 'goalie'},
+        'ryan':
+        {'wins': 9, 'losses': 3, 'pos': 'striker'},
+        }
+
+# Now what if we were to include add'l prefs?
+foos = {'joe':
+        {'wins': 5, 'losses': 5, 'prefs': {'pos': 'goalie'}},
+        'eric':
+        {'wins': 7, 'losses': 5, 'prefs': {'pos': 'goalie'}},
+        'ryan':
+        {'wins': 9, 'losses': 3, 'prefs': {'pos': 'striker'}},
+        }
+# Getting ugly now...
+
+# Instead, implement nested dicts as helper classes
+class WinLossRecord:
+    def __init__(self, wins, losses):
+        self.wins = wins
+        self.losses = losses
+
+    def total_games_played(self):
+        return self.wins + self.losses
+
+def FoosEntry:
+    def __init__(self, name, win_loss_record, prefs):
+        self.name = name
+        self.win_loss_record = win_loss_record
+        self.prefs = prefs
+
+foosbook = []
+foosbook.append(FoosEntry('joe', WinLossRecord(5,5), prefs=None))
+foosbook.append(FoosEntry('eric', WinLossRecord(7,5), prefs=None))
+foosbook.append(FoosEntry('ryan', WinLossRecord(9,3), prefs=None))
 
