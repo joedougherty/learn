96bb5a018069d8169093241a74eaa50b5f42781a
Joe Dougherty <joseph.dougherty@gmail.com>
2017-01-13 17:08:34 -0500

    User super()

---
+++ effective_python/ch03/super_vs__init__.py
@@ -0,0 +1,12 @@
+"""
+
+When creating a child class from a parent class,
+use the built-in super() to instantiate the parent class.
+
+This makes the method resolution order more clear (in cases of
+multiple inheritance), and is simply a good practice.
+
+"""
+
+# TODO: Add concrete example
+
