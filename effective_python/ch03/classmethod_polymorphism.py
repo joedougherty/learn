25624fb0738f8f5c07d28c58528006d6c93dbb38
Joe Dougherty <joseph.dougherty@gmail.com>
2017-01-15 18:48:43 -0500

    Provides some additional exposition around the generic mapreduce implementation

--- effective_python/ch03/classmethod_polymorphism.py
+++ effective_python/ch03/classmethod_polymorphism.py
@@ -29,3 +29,61 @@ class LineCountWorker(Worker):
     def reduce(self, other):
         self.result += other.result
 
+"""
+How does one manage the orchestration?
+"""
+
+def generate_inputs(data_dir):
+    for name in os.listdir(data_dir):
+        yield PathInputData(os.path.join(data_dir, name))
+
+def create_workers(input_list):
+    return [LineCountWorker(i) for i in input_list]
+
+def execute(workers):
+    threads = [Thread(target=w.map) for w in workers]
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+
+    first, rest = workers[0], workers[1:]
+    for worker in rest:
+        first.reduce(worker)
+    return first.result
+
+def mapreduce(data_dir):
+    """
+    More concisely:
+
+    return execute(create_workers(generate_inputs(data_dir)))
+    """
+    inputs = generate_inputs(data_dir)
+    workers = create_workers(inputs)
+    return execute(workers)
+
+"""
+So what's so bad about this?
+    * PathInputData is a hard dependency in generate_inputs()
+    * LineCountWorker is a hard dependency in create_workers()
+
+It is a _version_ of mapreduce, but too specific. 
+
+The eventual goal is to be able to do this:
+"""
+
+def mapreduce(worker_class, input_class, config):
+    workers = worker_class.create_workers(input_class, config)
+    return execute(workers)
+
+"""
+To be able to use this, we need:
+    * The worker class to expose a class method (create_workers) that will
+        handle the creation of the requisite worker instances
+    * The input class to expose a class method (generate_inputs) that will
+        handle the creation of the requisite input instances
+        (called by create_workers)
+"""
+
+# TODO: Give rundown of what happens here!
+
---
+++ effective_python/ch03/generic_classmethod_polymorphism.py
@@ -0,0 +1,23 @@
+class GenericInputData(object):
+    def read(self):
+        raise NotImplementedError
+
+    @classmethod
+    def generate_inputs(cls, config):
+        raise NotImplementedError
+
+class PathInputData(GenericInputData):
+    def __init__(self, path):
+        super().__init__()
+        self.path = path
+
+    def read(self):
+        return open(self.path).read()
+
+    @classmethod
+    def generate_inputs(cls, config):
+        data_dir = config['data_dir']
+        for name in os.listdir(data_dir):
+            yield cls(os.path.join(data_dir, name))
+
+
