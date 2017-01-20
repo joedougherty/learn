321f4139aca5ae75a3dc492be9c0021b106a7498
Joe Dougherty <joseph.dougherty@gmail.com>
2017-01-19 19:22:08 -0500

    First pass at thorough description complete!

--- effective_python/ch03/classmethod_polymorphism.py
+++ effective_python/ch03/classmethod_polymorphism.py
@@ -1,6 +1,10 @@
+import os
+import Thread
+
 class InputData(object):
     def read(self):
         raise NotImplementedError
+
 
 class PathInputData(InputData):
     def __init__(self, path):
@@ -9,6 +13,7 @@ class PathInputData(InputData):
 
     def read(self):
         return open(self.path).read()
+
 
 class Worker(object):
     def __init__(self, input_data):
@@ -20,6 +25,7 @@ class Worker(object):
 
     def reduce(self, other):
         raise NotImplementedError
+
 
 class LineCountWorker(Worker):
     def map(self):
@@ -33,12 +39,15 @@ class LineCountWorker(Worker):
 How does one manage the orchestration?
 """
 
+
 def generate_inputs(data_dir):
     for name in os.listdir(data_dir):
         yield PathInputData(os.path.join(data_dir, name))
 
+
 def create_workers(input_list):
     return [LineCountWorker(i) for i in input_list]
+
 
 def execute(workers):
     threads = [Thread(target=w.map) for w in workers]
@@ -52,7 +61,8 @@ def execute(workers):
         first.reduce(worker)
     return first.result
 
-def mapreduce(data_dir):
+
+def mapreduce_first_pass(data_dir):
     """
     More concisely:
 
@@ -67,10 +77,11 @@ So what's so bad about this?
     * PathInputData is a hard dependency in generate_inputs()
     * LineCountWorker is a hard dependency in create_workers()
 
-It is a _version_ of mapreduce, but too specific. 
+It is a _version_ of mapreduce, but too specific.
 
 The eventual goal is to be able to do this:
 """
+
 
 def mapreduce(worker_class, input_class, config):
     workers = worker_class.create_workers(input_class, config)
@@ -84,6 +95,7 @@ To be able to use this, we need:
         handle the creation of the requisite input instances
         (called by create_workers)
 """
+
 
 class GenericWorker(object):
     def map(self):
@@ -99,6 +111,7 @@ class GenericWorker(object):
             workers.append(cls(input_data))
         return workers
 
+
 class LineCountWorker(GenericWorker):
     def map(self):
         data = self.input_data.read()
@@ -113,9 +126,10 @@ class LineCountWorker(GenericWorker):
 When this line is called, the following occurs:
 
     * The create_workers classmethod is called
-        The concrete classes in question here are: 
+        The concrete classes in question here are:
             * LineCountWorker
-            * PathInputData 
+            * PathInputData
+
     * The generate_inputs classmethod is called (with config dict)
         Assume the path given by the config dict contains 3 files:
             * a.txt
@@ -125,5 +139,11 @@ When this line is called, the following occurs:
         generate_inputs yields a generator object that produces
         instances of the PathInputData class, one per file found
         in directory given by config dict
+
+    * A worker is created for each input [cls(input_data)] and a list
+        of all workers is returned
+
+    * Lastly, execute fires up a thread per worker, and returns
+        the accumulated results of the map calls
 
 """
