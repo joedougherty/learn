28d35a8e42bab822a8afeb39996794e8c7f8765d
Joe Dougherty <joseph.dougherty@gmail.com>
2017-01-21 19:08:05 -0500

    Mixins!

---
+++ effective_python/ch03/mulitple_inheritance_for_mixins.py
@@ -0,0 +1,13 @@
+"""
+Best to avoid multiple inheritance if possible.
+
+Considers _mix-ins_.
+
+Mix-ins:
+    * are classes but...
+      * do not have their own instance attributes
+      * do not require __init__ to be run
+
+"""
+
+# TODO: provide concrete example
